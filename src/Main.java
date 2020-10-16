5f9fef4f1e25608d3c7128018ebe6af2fb2c0f51
arasopraza <aras.opraza@gmail.com>
2020-10-16 21:12:54 +0700

    Initial Commit :tada:

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @author
+ * NAMA     : Arsy Opraza Akma
+ * KELAS    : IF1
+ * NIM      : 10119001
+ * Deskripsi Program: Program ini berisi tentang operator logika
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur > 17);
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+}
